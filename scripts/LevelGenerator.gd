a9ffc61c5b4128a52e6a77700441b1d9b0f50ddd
Sean Sinclair <sean.tollefsen.sinclair@gmail.com>
2023-12-22 12:44:17 +0100

    First failed attempt at a script that enables endless mode.

--- scripts/LevelGenerator.gd
+++ scripts/LevelGenerator.gd
@@ -8,24 +8,40 @@ var min_offset_z := -10
 var max_offset_x := 10
 var max_offset_z := 10
 
+var amount_created := 0
+
 func AddHorizontalOffsetToVector(vector):
 	var offset_x := randf_range(min_offset_x, max_offset_x)
 	var offset_z := randf_range(min_offset_z, max_offset_z)
 	
 	var final_vector := Vector3(offset_x, vector.y, offset_z)
 	return final_vector
+	
+func PlacePlatform(platform):
+	# Calculate position of platform
+	var platform_position := Vector3(0, (vertical_offset*amount_created)*-1, 0)
+	
+	# Do not add horizontal offset if it is the first one as the player spawns in the center.
+	if amount_created != 0:
+		platform_position = AddHorizontalOffsetToVector(platform_position)
+		
+	# Move and rotate platform. 
+	platform.position = platform_position
+	platform.rotate_y(randf_range(-360, 360))
 
-func _ready():
-	# Generate the first 10 platforms
-	for i in range(10):
+func GenerateNewPlatforms(amount: int):
+	for i in range(amount):
+		# Create the platform
 		var platform = platform_scene.instantiate()
 		get_parent().add_child.call_deferred(platform)
-		var platform_position := Vector3(0, (vertical_offset*i)*-1, 0)
-		if i != 0:
-			platform_position = AddHorizontalOffsetToVector(platform_position)
-		platform.transform.origin = platform_position
-		platform.rotate_y(randf_range(-360, 360))
+		
+		PlacePlatform(platform)
+		
+		amount_created += 1
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _ready():
+	GenerateNewPlatforms(10)
+
+func _on_area_3d_body_entered(body):
+	print("Oh no")
+	GenerateNewPlatforms(10)
