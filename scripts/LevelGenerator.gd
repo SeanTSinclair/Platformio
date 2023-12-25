3d1969f0e304e67c0a094bc757f06b5976e817d4
Sean Sinclair <sean.tollefsen.sinclair@gmail.com>
2023-12-25 14:42:08 +0100

    Found a programmatic way of creating the cylinder and move_trigger. The collider for the cylinder still isn't working.

--- scripts/LevelGenerator.gd
+++ scripts/LevelGenerator.gd
@@ -2,20 +2,71 @@ extends Node
 class_name LevelGenerator
 
 var platform_scene := preload("res://objects/platform.tscn")
-var vertical_offset := 10
+signal on_should_move_cylinder
 
-var min_offset_x := -10
-var min_offset_z := -10
-var max_offset_x := 10
-var max_offset_z := 10
+@export var vertical_offset := 10
+@export var horizontal_offset := 10
+@export var cylinder_height := 400
+@export var cylinder_radius := 35
+@export var cylinder_thickness := 0.2
+@export var cylinder_sides := 32
 
 var amount_created := 0
-
 var spawned_platforms := []
 
-func AddHorizontalOffsetToVector(vector):
-	var offset_x := randf_range(min_offset_x, max_offset_x)
-	var offset_z := randf_range(min_offset_z, max_offset_z)
+func GenerateCylinderObject():
+	#https://gamedevacademy.org/csgcylinder3d-in-godot-complete-guide/
+	var cylinder = Node3D.new()
+	
+	# The physical shapes of the cylinder.
+	var outer_cylinder = CSGCylinder3D.new()
+	outer_cylinder.name = "outer_cylinder"
+	var inner_cylinder = CSGCylinder3D.new()
+	inner_cylinder.name = "inner_cylinder"
+	# The collision shape of the cylinder.
+	var collision_shape = CollisionShape3D.new()
+	collision_shape.name = "collision_shape"
+	var cylinder_shape = CylinderShape3D.new()
+	# For moving the cylinder when the player enters the area.
+	var move_area = Area3D.new()
+	move_area.name = "move_area"
+	var move_collider = CollisionShape3D.new()
+	move_collider.name = "move_collider"
+	var move_shape = CylinderShape3D.new()
+	
+	# Create the cylinder
+	outer_cylinder.height = cylinder_height
+	outer_cylinder.radius = cylinder_radius
+	outer_cylinder.sides = cylinder_sides
+	inner_cylinder.height = cylinder_height + cylinder_thickness  # Slightly taller for clean subtraction
+	inner_cylinder.radius = cylinder_radius - cylinder_thickness  # Smaller radius for inner space
+	inner_cylinder.sides = cylinder_sides
+	inner_cylinder.operation = CSGShape3D.OPERATION_SUBTRACTION
+	
+	cylinder.add_child(outer_cylinder)
+	outer_cylinder.add_child(inner_cylinder)
+	
+	cylinder_shape.height = inner_cylinder.height
+	cylinder_radius = inner_cylinder.radius
+	collision_shape.shape = cylinder_shape
+	move_shape.height = 4
+	move_shape.radius = cylinder_radius
+	move_collider.shape = move_shape
+	
+	move_area.area_entered.connect(_on_area_entered)
+	outer_cylinder.add_child(collision_shape)
+	move_area.add_child(move_collider)
+	cylinder.add_child(move_area)
+	move_area.position.y = -90
+	
+	return cylinder
+
+func _on_area_entered(area):
+	on_should_move_cylinder.emit()
+
+func AddRandomHorizontalOffsetToVector(vector):
+	var offset_x := randf_range(horizontal_offset*-1, horizontal_offset)
+	var offset_z := randf_range(horizontal_offset*-1, horizontal_offset)
 	
 	var final_vector := Vector3(offset_x, vector.y, offset_z)
 	return final_vector
@@ -26,7 +77,7 @@ func PlacePlatform(platform):
 	
 	# Do not add horizontal offset if it is the first one as the player spawns in the center.
 	if amount_created != 0:
-		platform_position = AddHorizontalOffsetToVector(platform_position)
+		platform_position = AddRandomHorizontalOffsetToVector(platform_position)
 		
 	# Move and rotate platform. 
 	platform.position = platform_position
--- scripts/LevelManager.gd
+++ scripts/LevelManager.gd
@@ -3,33 +3,16 @@ extends Node
 @export var cylinder_move_distance := 90
 
 var level_generator: LevelGenerator
-var has_entered_move_trigger: bool = false
 var cylinder: Node3D
 
 func _ready():
-	cylinder = %Cylinder
 	level_generator = %LevelGenerator
+	level_generator.connect("on_should_move_cylinder", _move_cylinder)
+	cylinder = level_generator.GenerateCylinderObject()
+	%Cylinder.add_child(cylinder)
+	level_generator.GenerateNewPlatforms(20)
+	
+func _move_cylinder():
+	cylinder.position.y -= cylinder_move_distance
 	level_generator.GenerateNewPlatforms(20)
 
-func _on_area_3d_body_entered(body):
-	if not has_entered_move_trigger:
-		print("moving platform")
-		cylinder.position.y -= cylinder_move_distance
-		has_entered_move_trigger = true
-		level_generator.GenerateNewPlatforms(20)
-
-func _on_area_3d_body_exited(body):
-	print("Body exited")
-	has_entered_move_trigger = false
-
-
-func _on_area_3d_area_entered(area):
-	if not has_entered_move_trigger:
-		print("moving platform")
-		cylinder.position.y -= cylinder_move_distance
-		has_entered_move_trigger = true
-		level_generator.GenerateNewPlatforms(20)
-
-func _on_area_3d_area_exited(area):
-	print("Area exited")
-	has_entered_move_trigger = false
